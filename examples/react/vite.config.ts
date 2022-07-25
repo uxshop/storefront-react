4605e5cd154bb3193de226d465b64b446a92c959
diovaniBelleboni <diovani@dooca.com.br>
2022-07-25 16:52:23 -0300

    hotfix: ajuste import plugin

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { doocaPlugin } from 'lw-storefront/lib/plugin/dc-vite'
+import { doocaPlugin } from '@uxshop/storefront-core/dist/plugin/dc-vite'
 import config from './dooca.config'
 
 export default defineConfig({
-  plugins: [doocaPlugin(config), react()]
+    plugins: [doocaPlugin(config), react()]
 })
