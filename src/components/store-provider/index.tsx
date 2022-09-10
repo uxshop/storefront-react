9a22e0e72271ae7a971bb0630cf80092c84ea5a7
tiagotermedooca <89025816+tiagotermedooca@users.noreply.github.com>
2022-09-10 10:32:27 -0300

    fix: base path

--- src/components/store-provider/index.tsx
+++ src/components/store-provider/index.tsx
@@ -3,10 +3,9 @@ import { BrowserRouter } from 'react-router-dom'
 import RootProvider from './root-provider'
 
 export function StoreProvider(props: any) {
-    const basename = props.basename ?? shop.base_path
-    return (
-        <BrowserRouter basename={basename}>
-            <RootProvider pages={props.pages} routes={props.routes} />
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter basename={props.basename}>
+      <RootProvider pages={props.pages} routes={props.routes} />
+    </BrowserRouter>
+  )
 }
