e43f37358e1295d26349ea193c212845a6fe8ab4
diovaniBelleboni <diovani@dooca.com.br>
2022-09-20 21:49:37 -0300

    refactor: ajustes hook product

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,28 +1,28 @@
 interface shop {
-    token: string
-    api_url: string
-    api_editor: string
-    domain: string
-    base_path?: string
-    mock?: {
-        apps?: any
-        category?: any
-        categoryTree?: any
-        blogCategory?: any
-        blogPost?: any
-        brand?: any
-        cart?: any
-        landing_pages?: any
-        menu?: any
-        pages?: any
-        page?: any
-        scripts?: any
-        sections?: any
-        settings?: any
-        shop?: any
-        showcase?: any
-        user?: any
-    }
+  token: string
+  api_url: string
+  api_editor: string
+  domain: string
+  base_path?: string
+  mock?: {
+    apps?: any
+    category?: any
+    categoryTree?: any
+    blogCategory?: any
+    blogPost?: any
+    brand?: any
+    cart?: any
+    landing_pages?: any
+    menu?: any
+    pages?: any
+    page?: any
+    scripts?: any
+    sections?: any
+    settings?: any
+    shop?: any
+    product?: any
+    user?: any
+  }
 }
 
 declare var shop: shop
