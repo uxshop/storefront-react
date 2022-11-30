0e22f39233a935a38ecfc57fa5d38f852bef1221
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2022-11-30 09:54:38 -0300

    feat: adiciona trycatch e retorna objeto de erro

--- src/hooks/use-apps.tsx
+++ src/hooks/use-apps.tsx
@@ -1,22 +1,27 @@
 import { useEffect, useState } from 'react'
 import { AppService } from '@uxshop/storefront-core'
-import {AppFields} from '@uxshop/storefront-core/dist/modules/app/AppTypes'
+import { App, AppFields } from '@uxshop/storefront-core/dist/modules/app/AppTypes'
 
 interface AppHookParams {
   id: string
 }
 
 export function useApps({ id }: AppHookParams, fields?: AppFields[]): any {
-  const [apps, setApps] = useState<any>()
+  const [data, setData] = useState<App>()
+  const [error, setError] = useState()
 
   async function getById({ id }: AppHookParams, fields?: AppFields[]) {
-    const result = await AppService.getById(id, fields)
-    setApps(result)
+    try {
+      const result = await AppService.getById(id, fields)
+      setData(result)
+    } catch (error) {
+      setError(error)
+    }
   }
 
   useEffect(() => {
     getById({ id }, fields)
   }, [])
 
-  return apps
+  return { data, error }
 }
