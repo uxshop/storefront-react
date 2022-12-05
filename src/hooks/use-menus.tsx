adf38e1477c37f81f2b02915d62e9b9e6081fa67
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2022-12-05 11:59:17 -0300

    feat: adiciona useEffect para campo com dependencia de id

--- src/hooks/use-menus.tsx
+++ src/hooks/use-menus.tsx
@@ -1,17 +1,18 @@
 import { MenuService } from '@uxshop/storefront-core'
 import { MenuFields } from '@uxshop/storefront-core/dist/modules/menu/MenuTypes'
+import { useEffect } from 'react'
 
 interface MenuHookParams {
   id?: string
 }
 
 export function useMenus({ id }: MenuHookParams, fields?: MenuFields[]): any {
-  function getOneById(id: string, fields?: MenuFields[]) {
-    let result = {
-      data: null,
-      error: null
-    }
+  let result = {
+    data: null,
+    error: null
+  }
 
+  function getOneById(id: string, fields?: MenuFields[]) {
     MenuService.getById(id, fields)
       .then(response => (result.data = response))
       .catch(error => (result.error = error))
@@ -20,11 +21,6 @@ export function useMenus({ id }: MenuHookParams, fields?: MenuFields[]): any {
   }
 
   function getList(fields?: MenuFields[]) {
-    let result = {
-      data: null,
-      error: null
-    }
-
     MenuService.getList(fields)
       .then(response => (result.data = response))
       .catch(error => (result.error = error))
@@ -32,5 +28,9 @@ export function useMenus({ id }: MenuHookParams, fields?: MenuFields[]): any {
     return result
   }
 
-  return id ? getOneById(id, fields) : getList(fields)
+  useEffect(() => {
+    id ? getOneById(id, fields) : getList(fields)
+  }, [id])
+
+  return result
 }
