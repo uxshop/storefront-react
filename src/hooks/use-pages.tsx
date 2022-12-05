89fc621c8fac9e791081c2e3c3809f8a9b8ec86b
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2022-12-05 09:46:45 -0300

    fix: altera nomenclatura de array PageFields

--- src/hooks/use-pages.tsx
+++ src/hooks/use-pages.tsx
@@ -7,17 +7,17 @@ interface PageHookParams {
   slug?: string
 }
 
-export function usePages({ id, slug }: PageHookParams, fields?: Array<PageFields>): any {
+export function usePages({ id, slug }: PageHookParams, fields?: PageFields[]): any {
   const [pages, setPages] = useState<any>()
 
-  async function getOne({ id, slug }: PageHookParams, fields?: Array<PageFields>) {
+  async function getOne({ id, slug }: PageHookParams, fields?: PageFields[]) {
     const service = id ? PagesService.getById : PagesService.getBySlug
     const param = id ?? slug
     const result = await service(param, fields)
     setPages(result)
   }
 
-  async function getList(fields?: Array<PageFields>) {
+  async function getList(fields?: PageFields[]) {
     const result = await PagesService.getList(fields)
     setPages(result)
   }
