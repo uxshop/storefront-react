3a4091278cdc1f9d0258227734b9f59ed40301ab
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2022-12-08 12:41:14 -0300

    fix: adiciona nova estrutura de retorno em useBlogPosts

--- src/hooks/use-blog-posts.tsx
+++ src/hooks/use-blog-posts.tsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from 'react'
 import { BlogPostService } from '@uxshop/storefront-core'
 import {
   BlogPostFields,
   BlogPostListFilter
 } from '@uxshop/storefront-core/dist/modules/blog/post/BlogPostTypes'
+import { HookData } from './types/HookData'
 
 interface BlogPostHookParams extends Omit<BlogPostListFilter, 'page' | 'fastSearch'> {
   page?: number
@@ -15,36 +17,34 @@ export function useBlogPosts(
   { id, slug, page, first, postCategoryId, searchTerm }: BlogPostHookParams,
   fields?: BlogPostFields[]
 ): any {
+  const [status, setStatus] = useState<HookData>({
+    loading: false,
+    data: null
+  })
+
   function getOne() {
-    let result = {
-      data: null,
-      error: null
-    }
+    setStatus({ loading: true })
 
     const service = id ? BlogPostService.getById : BlogPostService.getBySlug
     const param = id ?? slug
     service(param, fields)
-      .then(reponse => (result.data = reponse))
-      .catch(error => (result.error = error))
-
-    return result
+      .then(response => setStatus({ loading: false, data: response }))
+      .catch(error => setStatus({ loading: false, error }))
   }
 
   function getList() {
-    let result = {
-      data: null,
-      error: null
-    }
+    setStatus({ loading: true })
 
     const filter = { page, first, postCategoryId }
     const fastSearch = searchTerm ? { fastSearch: { queryString: searchTerm } } : {}
 
     BlogPostService.getList({ ...filter, ...fastSearch }, fields)
-      .then(response => (result.data = response))
-      .catch(error => (result.error = error))
-
-    return result
+      .then(response => setStatus({ loading: false, data: response }))
+      .catch(error => setStatus({ loading: false, error }))
   }
 
-  return id || slug ? getOne() : getList()
+  useEffect(() => {
+    id || slug ? getOne() : getList()
+  })
+  return { ...status }
 }
