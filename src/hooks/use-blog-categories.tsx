f80ce00d3ac11dd6033c343bf9d96fa059d7f06d
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2022-12-08 12:46:46 -0300

    fix: adiciona nova estrutura de retorno de dados no useBlogCategories

--- src/hooks/use-blog-categories.tsx
+++ src/hooks/use-blog-categories.tsx
@@ -1,5 +1,7 @@
 import { BlogCategoryService } from '@uxshop/storefront-core'
 import { BlogCategoryFields } from '@uxshop/storefront-core/dist/modules/blog/category/BlogCategoryTypes'
+import { useEffect, useState } from 'react'
+import { HookData } from './types/HookData'
 
 interface BlogCategoryHookParams {
   id?: string
@@ -10,33 +12,32 @@ export function useBlogCategories(
   getOneFilter?: BlogCategoryHookParams,
   fields?: BlogCategoryFields[]
 ): any {
+  const [status, setStatus] = useState<HookData>({
+    loading: false,
+    data: null
+  })
+
   function getOne() {
-    let result = {
-      data: null,
-      error: null
-    }
+    setStatus({ loading: true })
+
     const service = getOneFilter.id ? BlogCategoryService.getById : BlogCategoryService.getBySlug
     const param = getOneFilter.id ?? getOneFilter.slug
 
     service(param, fields)
-      .then(response => (result.data = response))
-      .catch(error => (result.error = error))
-
-    return result
+      .then(response => setStatus({ loading: false, data: response }))
+      .catch(error => setStatus({ loading: false, error }))
   }
 
   function getList() {
-    let result = {
-      data: null,
-      error: null
-    }
+    setStatus({ loading: true })
 
     BlogCategoryService.getList(fields)
-      .then(response => (result.data = response))
-      .catch(error => (result.error = error))
-
-    return result
+      .then(response => setStatus({ loading: false, data: response }))
+      .catch(error => setStatus({ loading: false, error }))
   }
+  useEffect(() => {
+    getOneFilter?.id || getOneFilter?.slug ? getOne() : getList()
+  }, [])
 
-  return getOneFilter?.id || getOneFilter?.slug ? getOne() : getList()
+  return { ...status }
 }
