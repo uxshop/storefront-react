45f9629f81aa6d55e49a6109117e7f45b7a75ebc
vitorbruski <vitorgbruski@gmail.com>
2022-12-14 09:33:02 -0300

    fix: corrigido componentes que nao recebiam filhos

--- src/components/ux/ux-showcase/index.tsx
+++ src/components/ux/ux-showcase/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { UxShowcaseProps } from './types'
 import { defineCustomElement as defineUxShowcase } from '@uxshop/storefront-components/dist/components/ux-showcase'
 
-export function UxShowcase() {
+export function UxShowcase({ children }: UxShowcaseProps) {
   defineUxShowcase()
 
-  return React.createElement('ux-showcase')
+  return React.createElement('ux-showcase', null, children)
 }
