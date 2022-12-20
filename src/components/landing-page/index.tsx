1731207f76b1579f44be7350957ac0ad24bb18ac
simaomeyrerdooca <simao@doocacommerce.com.br>
2022-12-19 22:36:21 -0300

    feat: index dos componentes de UX

---
+++ src/components/landing-page/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function LandingPage() {
+  return <div>index</div>
+}
---
+++ src/components/ux/index.tsx
@@ -0,0 +1,15 @@
+import { UxBanners } from '../ux/ux-banners'
+import { UxCarousel } from '../ux/ux-carousel'
+import { UxShowcase } from '../ux/ux-showcase'
+import { UxTestimonials } from '../ux/ux-testimonials'
+import { UxTextImage } from '../ux/ux-text-image'
+import { UxVideo } from '../ux/ux-video'
+
+export default {
+  banners: UxBanners,
+  carrossel: UxCarousel,
+  showcase: UxShowcase,
+  testimonials: UxTestimonials,
+  'text-image': UxTextImage,
+  video: UxVideo
+}
