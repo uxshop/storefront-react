f90dac2613347867f39be58db11484e9cab8d115
simaomeyrerdooca <simao@doocacommerce.com.br>
2022-12-20 22:24:12 -0300

    feat: criado componente de LandingPage loader

--- src/components/landing-page/index.tsx
+++ src/components/landing-page/index.tsx
@@ -1,5 +1,32 @@
 import React from 'react'
+import { useLandingPages } from '../../hooks/use-landing-pages'
+import { SectionLoader } from '../section-loader'
+import components from '../ux'
 
-export function LandingPage() {
-  return <div>index</div>
+export function LandingPage(): JSX.Element {
+  const landingPageData = useLandingPages({ id: String(2802) })
+  return (
+    <>
+      {landingPageData?.data?.content?.sections &&
+        landingPageData?.data?.content?.sections.map(
+          ({ id, schema, blocks, settings, type, disabled }): JSX.Element => {
+            const component = components[schema]
+
+            return (
+              <React.Fragment key={id}>
+                {type === 'content' && (
+                  <SectionLoader
+                    id={id}
+                    component={component}
+                    settings={settings}
+                    blocks={blocks}
+                    disabled={disabled}
+                  />
+                )}
+              </React.Fragment>
+            )
+          }
+        )}
+    </>
+  )
 }
