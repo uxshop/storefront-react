af73463e48c26cc51325b71531056f1207c6f9dd
Keven de Sousa Silva <keven.sousa@bagy.com.br>
2022-12-27 15:05:10 -0300

    fix: ajusta compentent de UxTestimonials para retornar depoimentos

--- src/components/landing-page-sections/ux-testimonials/index.tsx
+++ src/components/landing-page-sections/ux-testimonials/index.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { UxTestimonialsProps } from './types'
 import { defineCustomElement as defineUxTestimonials } from '@uxshop/storefront-components/dist/components/ux-testimonials'
 
-export function UxTestimonials({ settings }: UxTestimonialsProps) {
+export function UxTestimonials({ settings, blocks }: UxTestimonialsProps) {
   const dataProps = {
     'data-description': settings.dataDescription,
-    'data-testimonials': settings.dataTestimonials,
+    'data-testimonials': JSON.stringify(blocks.map(block => block.settings)),
     'data-title': settings.dataTitle
   }
 
   defineUxTestimonials()
-
   return React.createElement('ux-testimonials', dataProps)
 }
--- src/components/landing-page-sections/ux-testimonials/types.ts
+++ src/components/landing-page-sections/ux-testimonials/types.ts
@@ -1,9 +1,19 @@
 export interface UxTestimonialsSettings {
-  dataDescription: string
-  dataTestimonials: string
-  dataTitle: string
+  dataDescription?: string
+  dataTestimonials?: string
+  dataTitle?: string
+}
+export interface UxTestimonialsBlocks {
+  settings?: UxTestimonialsBlocksSettings
 }
 
 export interface UxTestimonialsProps {
+  blocks: UxTestimonialsBlocks[]
   settings: UxTestimonialsSettings
+}
+
+export interface UxTestimonialsBlocksSettings {
+  name?: string
+  icon?: string
+  message?: string
 }
